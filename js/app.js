43dfc97a21689ff072f537432d7eb31147b3397d
Sascha Schmittinger <sascha-schmittinger@outlook.com>
2020-08-12 11:05:33 +0200

    first

---
+++ js/app.js
@@ -0,0 +1,36 @@
+let bars = document.querySelector('.menu-toggle');
+let body = document.querySelector('body');
+
+bars.addEventListener('click', function () {
+    body.classList.toggle('open');
+})
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
